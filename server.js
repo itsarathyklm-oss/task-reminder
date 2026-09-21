56f5b32f23a1e9d273e97ea88e370da63b278fca
itsarathyklm-oss <itsarathyklm@gmail.com>
2026-09-21 12:46:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ app.use(helmet({
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true, limit: '1mb' }));
 app.use(cors({
-    origin: process.env.ALLOWED_ORIGIN || 'https://task-reminder-xrvn.onrender.com',
+    origin: process.env.ALLOWED_ORIGIN || '*',
     credentials: true
 }));
 app.use(express.static('public'));
